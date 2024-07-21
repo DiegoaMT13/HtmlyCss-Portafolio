bca954cacc04403d7086532bf48e5cb0dc37220d
Diego Armando Martinez Tapias <damt13a@gmail.com>
2024-07-21 14:41:37 -0500

    portafolio

---
+++ app.js
@@ -0,0 +1,16 @@
+
+var swiper = new Swiper(".mySwiper",{
+    effect:"coverflow",
+    grabCursor:true,
+    centerdSlides:true,
+    slidesPerView:"auto",
+    loop:true,
+    coverflowEffect: {
+        depth:500,
+        modifer:1,
+        slidesShadows:true,
+        rotate:0,
+        stretch:0,
+
+    }
+});
